77e1bdd357b725e2edd86fe0620ac2a4eba949d9
alexmagno6m <alexacevedog@gmail.com>
2023-05-30 23:48:47 -0500

    new files modified

--- pack_example.py
+++ pack_example.py
@@ -4,7 +4,9 @@ from tkinter.ttk import *
 root = Tk()
 root.title('Login')
 root.geometry("350x220")
-
+style = Style()
+style.configure('TLabel', font=('Arial', 16))
+style.configure('TButton', font=('Arial', 12), background="#ccc")
 
 Label(text='Username:').pack(anchor=W, padx=10, pady=5, fill=X)
 Entry().pack(anchor=W, padx=10, pady=5, fill=X)
@@ -14,5 +16,17 @@ Entry(show="*").pack(anchor=W, padx=10, pady=5, fill=X)
 
 Button(text='Login').pack(anchor=W, padx=10, pady=5, side=LEFT)
 Button(text="Cancel").pack(anchor=W, padx=10, pady=5, side=LEFT)
+'''
+frame = Frame(root)
+frame.pack(anchor=SE, padx=10, pady=5)
+#evita que se ajuste la ventana al tamno del contenido
+frame.pack_propagate(0)
+
+
+Label(frame, text='Design by: Alexander Acevedo').pack(side=RIGHT)
+'''
+Label(text='Design by: Alexander Acevedo').place(anchor=SE, relx=1, rely=1, x=-10, y=-10)
+
 
 root.mainloop()
+
