c2f22d8087ea5c06e68614678a3c75ae5f1a3936
alexmagno6m <alexacevedog@gmail.com>
2024-02-29 00:21:26 -0500

    add file to checked character type

--- check_character.py
+++ check_character.py
@@ -1,5 +1,18 @@
-string = "hola 202"
+string = "hola"
+index = 0
+while True:
+        if string[index].isdecimal():
+            print('checked')
+            break
+        else:
+            index += 1
+            if index == len(string) - 1:
+                print('no decimal')
+                break
+'''
 for x in string:
     if x.isdigit():
         print("checked")
 
+print(string.find('h'))
+'''
