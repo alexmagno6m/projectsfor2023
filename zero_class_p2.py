c174954ad1a9e97edbf333b5cbfce1d597378746
alexmagno6m <alexacevedog@gmail.com>
2024-03-13 11:46:28 -0500

    new improvements

--- zero_class_p2.py
+++ zero_class_p2.py
@@ -5,6 +5,40 @@ names = ['Dave', 'Paula', 'Thomas', 'Lewis']
 names.insert(2, 'Andrea')
 print(names)
 
+# count elements
+print(names.count('Andrea'))
+
+# extend elements
+names2 = ['Pedro', 'Juan']
+names.extend(names2)
+print(names)
+
+# index return
+print(names.index('Pedro'))
+
+# pop elements, deleted by index, pedro deleted
+names.pop(5)
+print('pop', names)
+
+# remove lists, deleted by item name
+names.remove('Juan')
+print('remove', names)
+
+# sort, orders elements, descending reverse
+names.sort()
+print('order', names)
+names.sort(reverse=True)
+print('descending', names)
+
+# reverse object by index
+names.reverse()
+print('reverse', names)
+
+# max, min operate on numbers items
+numbers = [5, 20, 100, 45, 0]
+print(max(numbers))
+print(min(numbers))
+
 # create list
 names_2 = []
 names_3 = list('Alexander')
@@ -13,7 +47,7 @@ print(names_3)
 # sets
 brands = ['apple', 'samsung', 'huawei', 'hp', 'apple']
 brands2 = set(brands)
-print(brands2)
+print('brands2', brands2)
 
 brands3 = {'apple', 'samsung', 'huawei', 'hp'}
 
@@ -21,12 +55,12 @@ brands4 = {'apple', 'xiaomi', 'google', 'lenovo'}
 # union
 a = brands3 | brands4
 a1 = brands3.union(brands4)
-print('union',a, a1)
+print('union', a, a1)
 
 # intersection
 b = brands3 & brands4
 b1 = brands3.intersection(brands4)
-print('intersection',b, '-', b1)
+print('intersection', b, '-', b1)
 
 # difference
 c = brands3 - brands4
@@ -50,7 +84,7 @@ print(brands4)
 # adding multiple elements use update({new_item1, new_item2})
 # remove element, o raise error
 brands4.update({'alcatel', 'poco phone'})
-print(brands4)
+print('update', brands4)
 
 brands4.remove('motorola')
 print(brands4)
